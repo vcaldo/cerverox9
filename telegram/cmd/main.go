3f3ba0df8fef933803115cb95e1ee132d9972a1c
Vinicius Caldo <vcaldonoreply@github.com>
2024-12-19 02:19:09 +0100

    fix: remove unused voiceevents import from main.go

--- telegram/cmd/main.go
+++ telegram/cmd/main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 	"github.com/vcaldo/cerverox9/telegram/pkg/handlers"
-	"github.com/vcaldo/cerverox9/telegram/pkg/voiceevents"
 )
 
 func main() {
