74b7fad757dca6ad33176746f424efa3d91745db
Vinicius Caldo <vcaldonoreply@github.com>
2024-12-20 19:14:25 +0100

    refactor: enhance StatusHandler to display oncall and online user lists

--- telegram/pkg/handlers/handlers.go
+++ telegram/pkg/handlers/handlers.go
@@ -21,11 +21,25 @@ func StatusHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 		})
 		return
 	}
-	userSlice := strings.Split(usersList, ",")
-	usersListLineBreak := strings.Join(userSlice, "\n")
+	oncallUsersList := strings.Split(oncallUsers, ",")
+	oncallUsersListLinebreak := strings.Join(oncallUsersList, "\n")
+	onlineUsersList := strings.Split(onlineUsers, ",")
+	onlineUsersListLinebreak := strings.Join(onlineUsersList, "\n")
+
+	message := fmt.Sprintf(
+		"We have %d users having fun in the call.\n\n"+
+			"%s\n\n"+
+			"There are %d users who are one click away from having fun.\n\n"+
+			"%s",
+		oncallUsersCount,
+		oncallUsersListLinebreak,
+		onlineUsersCount,
+		onlineUsersListLinebreak,
+	)
+
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.Message.Chat.ID,
-		Text:   fmt.Sprintf("%d users are having fun in the call \n\n%s\n\n🥳🎊🎈🍾🎂🕺💃🎶🍻🥂", onlineUsers, usersListLineBreak),
+		Text:   message,
 	})
 }
 
--- telegram/pkg/stats/stats.go
+++ telegram/pkg/stats/stats.go
@@ -9,12 +9,8 @@ import (
 func GetVoiceCallStatus() (oncallUsersCount int64, oncallUsers string, onlineUsersCount int64, onlineUsers string, error error) {
 	dm := models.NewAuthenticatedDiscordMetricsClient()
 	guildID := os.Getenv("DISCORD_GUILD_ID")
-	onlineUsers, userList, err := dm.GetGuildStats(guildID)
-	if err != nil {
-		return 0, "", 0, "", err
-	}
 
-	oncallUsersCount, oncallUsers, err = dm.GetOncallUsers(guildID)
+	oncallUsersCount, oncallUsers, err := dm.GetOncallUsers(guildID)
 	if err != nil {
 		return 0, "", 0, "", err
 	}
